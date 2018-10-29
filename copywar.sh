91a1fa088d3869f6f165f8066d8ee746586605ec
root <root@ip-172-31-46-99.us-east-2.compute.internal>
2018-10-29 07:51:36 +0000

    added scrept file

---
+++ copywar.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+sudo wget http://3.16.55.187:8081/nexus/content/repositories/snapshots/com/efsavage/hello-world-war/1.0.0-SNAPSHOT/hello-world-war-1.0.0-20181029.043458-1.war  -P /usr/local/tomcat9/webapps/
